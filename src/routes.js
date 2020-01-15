dde15e06c3c7f600745ab315fceaacbf96a872db
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-01-14 22:55:42 -0300

    Criando novas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 
 import ClientController from './app/controllers/ClientController';
+import ItemController from './app/controllers/ItemController';
+import RentController from './app/controllers/RentController';
 
 const routes = new Router();
 
+routes.get('/clients', ClientController.index);
 routes.post('/clients', ClientController.store);
 routes.put('/clients/:id', ClientController.update);
+routes.delete('/clients/:id', ClientController.destroy);
+
+routes.post('/items', ItemController.store);
+
+routes.post('/rents', RentController.store);
 
 export default routes;
