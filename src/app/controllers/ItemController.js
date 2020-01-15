7c6af8ddcf3ac9f7435a286d862c65ea02bacd98
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-01-15 17:08:00 -0300

    CRUD para Item

--- src/app/controllers/ItemController.js
+++ src/app/controllers/ItemController.js
@@ -2,9 +2,43 @@ import Item from '../models/Item';
 
 class ItemController {
   async store(req, res) {
-    const item = await Item.create(req.body);
+    const { name, price } = req.body;
 
-    return res.json(item);
+    const itemExists = await Item.findOne({ where: { name } });
+
+    if (itemExists) {
+      return res.status(400).json({ error: 'This item already exists' });
+    }
+
+    const { id } = await Item.create({ name, price });
+
+    return res.json({ id, name, price });
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    if (req.body.name) {
+      const itemNameExists = await Item.findOne({
+        where: { name: req.body.name },
+      });
+
+      if (itemNameExists && itemNameExists.id !== Number(id)) {
+        return res.status(400).json({ error: 'This name already used' });
+      }
+    }
+
+    await Item.update(req.body, { where: { id } });
+
+    const { name, price } = await Item.findByPk(id);
+
+    return res.status(200).json({ name, price });
+  }
+
+  async index(req, res) {
+    const items = await Item.findAll();
+
+    return res.json(items);
   }
 }
 
