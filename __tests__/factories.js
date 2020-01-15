47ed688ae8c94dca1d68f880c07bec255f037a37
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-01-14 22:54:14 -0300

    Tests

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -2,11 +2,25 @@ import faker from 'faker';
 import factory from 'factory-girl';
 
 import Client from '../src/app/models/Client';
+import Item from '../src/app/models/Item';
+import Rent from '../src/app/models/Rent';
 
 factory.define('Client', Client, {
   name: faker.name.findName(),
   telefone: faker.phone.phoneNumber(),
   endereco: faker.address.streetAddress(),
+});
+
+factory.define('Item', Item, {
+  name: faker.commerce.product(),
+  price: faker.commerce.price(),
+});
+
+factory.define('Rent', Rent, {
+  // informar o client_id, e item_it na hora de usar
+  amount: faker.random.number(),
+  rent_price: faker.commerce.price(),
+  returned_at: null,
 });
 
 export default factory;
--- __tests__/integration/Client.test.js
+++ __tests__/integration/Client.test.js
@@ -9,79 +9,161 @@ describe('Client', () => {
     await truncate();
   });
 
-  it('should be able to register a client', async () => {
-    const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
+  describe('Create', () => {
+    it('should be able to register a client', async () => {
+      const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
 
-    const response = await request(app)
-      .post('/clients')
-      .send(client);
+      const response = await request(app)
+        .post('/clients')
+        .send(client);
 
-    expect(response.body).toHaveProperty('id');
+      expect(response.body).toHaveProperty('id');
+    });
+
+    it('should not be able to register with duplicated CPF', async () => {
+      const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
+
+      await request(app)
+        .post('/clients')
+        .send(client);
+
+      const response = await request(app)
+        .post('/clients')
+        .send(client);
+
+      expect(response.status).toBe(400);
+    });
   });
 
-  it('should not be able to register with duplicated CPF', async () => {
-    const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
+  describe('Update', () => {
+    it('should not be able to edit a client that does not exist', async () => {
+      const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
 
-    await request(app)
-      .post('/clients')
-      .send(client);
+      const response = await request(app)
+        .post('/clients')
+        .send(client);
 
-    const response = await request(app)
-      .post('/clients')
-      .send(client);
+      const editClient = await request(app)
+        .put(`/clients/${response.body.id + 1}`)
+        .send(client);
 
-    expect(response.status).toBe(400);
+      expect(editClient.body).toStrictEqual({
+        error: 'Client does not exists',
+      });
+    });
+
+    it('should be able to edit a client with a database id', async () => {
+      const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
+
+      const response = await request(app)
+        .post('/clients')
+        .send(client);
+
+      const clientEdit = await factory.attrs('Client', {
+        cpf: '000.000.000-00',
+      });
+
+      const editClient = await request(app)
+        .put(`/clients/${response.body.id}`)
+        .send(clientEdit);
+
+      expect(editClient.status).toBe(200);
+    });
+
+    it('should not be able to edit a client with a CPF already used', async () => {
+      let client = await factory.attrs('Client', { cpf: '111.111.111-11' });
+
+      await request(app)
+        .post('/clients')
+        .send(client);
+
+      client = await factory.attrs('Client', { cpf: '222.222.222-22' });
+
+      const response = await request(app)
+        .post('/clients')
+        .send(client);
+
+      const clientEdit = await factory.attrs('Client', {
+        cpf: '111.111.111-11',
+      });
+
+      const editClient = await request(app)
+        .put(`/clients/${response.body.id}`)
+        .send(clientEdit);
+
+      expect(editClient.status).toBe(400);
+    });
   });
 
-  it('should not be able to edit a client that does not exist', async () => {
-    const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
+  describe('List', () => {
+    it('it should be able to have a customer list', async () => {
+      const firstClient = await factory.attrs('Client', {
+        cpf: '111.111.111-11',
+      });
 
-    const response = await request(app)
-      .post('/clients')
-      .send(client);
+      await request(app)
+        .post('/clients')
+        .send(firstClient);
 
-    const editClient = await request(app)
-      .put(`/clients/${response.body.id + 1}`)
-      .send(client);
+      const secondClient = await factory.attrs('Client', {
+        cpf: '222.222.222-22',
+      });
 
-    expect(editClient.body).toStrictEqual({ error: 'Client does not exists' });
+      await request(app)
+        .post('/clients')
+        .send(secondClient);
+
+      const response = await request(app).get('/clients');
+
+      expect(response.body).toHaveLength(2);
+    });
   });
 
-  it('should be able to edit a client with a database id', async () => {
-    const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
+  describe('Destroy', () => {
+    it('should be able to delete a client who has no rent, even the rent has already been returned', async () => {
+      const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
 
-    const response = await request(app)
-      .post('/clients')
-      .send(client);
+      const creatResponse = await request(app)
+        .post('/clients')
+        .send(client);
 
-    const clientEdit = await factory.attrs('Client', { cpf: '000.000.000-00' });
+      const deleteResponse = await request(app).delete(
+        `/clients/${creatResponse.body.id}`
+      );
+      expect(deleteResponse.status).toBe(200);
+    });
 
-    const editClient = await request(app)
-      .put(`/clients/${response.body.id}`)
-      .send(clientEdit);
+    it('should be not able to delete a client who does not exists', async () => {
+      const deleteResponse = await request(app).delete('/clients/1');
+      expect(deleteResponse.status).toBe(400);
+    });
 
-    expect(editClient.status).toBe(200);
-  });
+    it('should not be able to delete a client who has a rent register', async () => {
+      const client = await factory.attrs('Client', { cpf: '111.111.111-11' });
+      const item = await factory.attrs('Item');
 
-  it('should not be able to edit a client with a CPF already used', async () => {
-    let client = await factory.attrs('Client', { cpf: '111.111.111-11' });
+      const creatClientResponse = await request(app) // cria um client
+        .post('/clients')
+        .send(client);
 
-    await request(app)
-      .post('/clients')
-      .send(client);
+      const creatItemResponse = await request(app) // cria um item
+        .post('/items')
+        .send(item);
 
-    client = await factory.attrs('Client', { cpf: '222.222.222-22' });
+      const rent = await factory.attrs('Rent', {
+        client_id: creatClientResponse.body.id,
+        item_id: creatItemResponse.body.id,
+      });
 
-    const response = await request(app)
-      .post('/clients')
-      .send(client);
+      await request(app) // cria rent
+        .post('/rents')
+        .send(rent);
 
-    const clientEdit = await factory.attrs('Client', { cpf: '111.111.111-11' });
+      const deleteClientResponse = await request(app).delete(
+        `/clients/${creatClientResponse.body.id}`
+      );
 
-    const editClient = await request(app)
-      .put(`/clients/${response.body.id}`)
-      .send(clientEdit);
-
-    expect(editClient.status).toBe(400);
+      expect(deleteClientResponse.status).toBe(400);
+    });
   });
 });
