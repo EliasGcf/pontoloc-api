50ab07968e63be88496c391298030a896b8c8557
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-01-20 16:03:25 -0300

    Ajuste na migration

--- src/database/migrations/20200120172045-create-orders.js
+++ src/database/migrations/20200120172045-create-orders.js
@@ -20,6 +20,10 @@ module.exports = {
         type: Sequelize.DOUBLE,
         allowNull: true,
       },
+      returned_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
