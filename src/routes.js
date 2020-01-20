7c329180f33b95e8615f3a96d2ca37daa595e756
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-01-20 16:50:44 -0300

    Ajuste nas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 
 import ClientController from './app/controllers/ClientController';
-import ItemController from './app/controllers/ItemController';
-import RentController from './app/controllers/RentController';
+import MaterialController from './app/controllers/MaterialController';
+import OrderController from './app/controllers/OrderController';
 
 const routes = new Router();
 
@@ -11,10 +11,10 @@ routes.post('/clients', ClientController.store);
 routes.put('/clients/:id', ClientController.update);
 routes.delete('/clients/:id', ClientController.destroy);
 
-routes.post('/items', ItemController.store);
-routes.put('/items/:id', ItemController.update);
-routes.get('/items', ItemController.index);
+routes.post('/materials', MaterialController.store);
+routes.put('/materials/:id', MaterialController.update);
+routes.get('/materials', MaterialController.index);
 
-routes.post('/rents', RentController.store);
+routes.post('/orders', OrderController.store);
 
 export default routes;
