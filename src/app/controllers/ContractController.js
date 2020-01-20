9ffd1898858926cd8e4d8e7ab01909ce66b92186
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-01-20 19:46:39 -0300

    Fix

--- src/app/controllers/ContractController.js
+++ src/app/controllers/ContractController.js
@@ -5,17 +5,17 @@ class ContractController {
   async store(req, res) {
     const { client_id, materials } = req.body;
 
-    const order = await Contract.create({ client_id });
+    const contract = await Contract.create({ client_id });
 
     materials.forEach(async material => {
       await ContractItem.create({
-        order_id: order.id,
+        contract_id: contract.id,
         material_id: material.id,
         quantity: material.quantity,
       });
     });
 
-    return res.json(order);
+    return res.json(contract);
   }
 }
 
