c00698f71bd59367c22ace46266f7d4ea3a76f46
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-02-06 15:48:23 -0300

    Add automatização do docker

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('../bootstrap');
 
 module.exports = {
   dialect: process.env.DB_DIALECT || 'postgres',
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'localhost',
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
